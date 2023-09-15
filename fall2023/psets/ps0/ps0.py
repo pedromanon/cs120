b4a80441af709c378fb9a34c2a246ea74f613e67
pedromanon <90016855+pedromanon@users.noreply.github.com>
2023-09-15 11:25:42 -0400

    Completed code for Pset0

--- fall2023/psets/ps0/ps0.py
+++ fall2023/psets/ps0/ps0.py
@@ -30,8 +30,17 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
-    pass
+    if v.left == None and v.right == None:
+        v.size = 1
+        return 1
+    left = 0
+    right = 0
+    if v.left:
+        left = calculate_sizes(v.left)
+    if v.right:
+        right = calculate_sizes(v.right)
+    v.size = 1 + left + right
+    return v.size
 
 #
 # Problem 1c
@@ -42,6 +51,27 @@ def calculate_sizes(v):
 # Output: A BTvertex that, if removed from the tree, would result
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
-def find_vertex(r): 
-    # Your code goes here
-    pass
+def find_vertex(r):
+    left = False
+    right = False
+    leftval = 0
+    rightval = 0
+    if r.left:
+        leftval = r.left.size
+        if r.left.size <= r.size/2:
+            left = True
+    else:
+        left = True
+    if r.right:
+        rightval = r.right.size
+        if r.right.size <= r.size/2:
+            right = True
+    else:
+        right = True
+    if left and right:
+        return r
+    else:
+        if leftval >= rightval:
+            return find_vertex(r.left)
+        else:
+            return find_vertex(r.right)
